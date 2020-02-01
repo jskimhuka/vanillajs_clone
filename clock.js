1b7f30438ba3446b38dc4c35d66f9b0ff86e1791
jskimhuka <kenesson6709@gmail.com>
2020-02-01 14:42:32 +0900

    user name part done

--- clock.js
+++ clock.js
@@ -6,10 +6,9 @@ function getTime() {
   const minutes = date.getMinutes();
   const hours = date.getHours();
   const seconds = date.getSeconds();
-  clockTitle.innerText = `${hours}:
-  ${minutes < 10 ? `0${minutes}` : minutes}:
-  ${seconds < 10 ? `0${seconds}` : seconds}
-  `;
+  clockTitle.innerText = `${hours}:${minutes < 10 ? `0${minutes}` : minutes}:${
+    seconds < 10 ? `0${seconds}` : seconds
+  }`;
 }
 
 function init() {
